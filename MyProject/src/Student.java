2a5a3f0a2b890e1509362ee9449f4b05bb7aa7c4
genady70 <kdu70@mail.ru>
2020-08-17 17:14:10 +0300

    17.082020

---
+++ MyProject/src/Student.java
@@ -0,0 +1,56 @@
+import java.util.Objects;
+
+public class Student {
+    String fio;
+    String data;
+    byte kurs;
+    double bursa;
+
+    public void setBursa(double bursa) {
+        this.bursa = bursa;
+    }
+
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public void setKurs(byte kurs) {
+        this.kurs = kurs;
+    }
+
+    public byte getKurs() {
+        return kurs;
+    }
+
+    public String getFio() {
+        return fio;
+    }
+
+    public double getBursa() {
+        return bursa;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return kurs == student.kurs &&
+                Double.compare(student.bursa, bursa) == 0 &&
+                Objects.equals(fio, student.fio) &&
+                Objects.equals(data, student.data);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fio, data, kurs, bursa);
+    }
+}
