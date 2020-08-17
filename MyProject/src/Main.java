5d8ef7c52d83827b7d21b92b7da6d888ca83566c
genady70 <kdu70@mail.ru>
2020-08-17 16:56:03 +0300

    var

---
+++ MyProject/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+	byte b=-15;
+	short s=12000;
+	int i=40000;
+	long l=87000L;
+	float f=4.25f;
+	double d=12.21;
+	char c='x';
+	String s1="str1";
+	boolean b1=true;
+		System.out.println(b+s);
+		System.out.println(	l+i);
+		System.out.println(d/f);
+		System.out.println(c+'y'+'z'+s1);
+		System.out.println(b1);
+    }
+}
